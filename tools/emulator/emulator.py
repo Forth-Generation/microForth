b024c36508893d4779d71107841e4b23c141ae68
prbaran <pbaran@baranhome.com>
2019-11-09 19:57:19 -0800

    Working lexer in emulator

--- tools/emulator/emulator.py
+++ tools/emulator/emulator.py
@@ -1,11 +1,42 @@
 import re
+import sys
 
-dictionary = []
+dictionary = [] 
 dstack = []
 rstack = []
 
+tokens = []
+token = '' 
 
-def addword(name):
+f = open(sys.argv[1])
+
+def get_token():
+    """
+    Fills tokens list with tokens
+
+    Args:
+        None
+    Returns:
+        False if end of file,
+        True otherwise
+    """
+
+    global tokens
+    global token
+
+    while not tokens:
+        line = f.readline()
+
+        if line == '':
+            return False
+
+        tokens = line.split()
+
+    token = tokens.pop(0)
+
+    return True 
+
+def add_word(name):
     """
     Adds word to the forth dictionary
 
@@ -16,6 +47,7 @@ def addword(name):
     """
 
     return
+
 def pushd(num):
     """
     Pushes a number onto the data stack
@@ -25,8 +57,13 @@ def pushd(num):
     Returns:
         None
     """
+
+    global dstack
+
+    dstack.append(num)
     
     return
+
 def popd():
     """
     Pops a number off the data stack
@@ -37,7 +74,12 @@ def popd():
         The integer popped off the data stack. If the data stack is empty it will return None. 
     """
 
+    global dstack
+
+    dstack.pop()
+
     return
+
 def execute(name):
     """
     Grabs the name and finds the dictionary word then executes
@@ -48,7 +90,8 @@ def execute(name):
         Boolean for success or failure
     """
     return
-def or():
+
+def or_func():
     """
     Or operation on the data stack.
     Pops off two numbers from the data stack and performs a bitwise OR
@@ -61,3 +104,6 @@ def or():
     """
 
     return
+
+while get_token():
+    print(token)
