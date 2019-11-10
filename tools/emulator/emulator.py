8954c7063b8728d4054d648d1f2b56df03fc6621
prbaran <pbaran@baranhome.com>
2019-11-09 18:04:41 -0800

    Added basic file structure and docstrings for emulator code

---
+++ tools/emulator/emulator.py
@@ -0,0 +1,63 @@
+import re
+
+dictionary = []
+dstack = []
+rstack = []
+
+
+def addword(name):
+    """
+    Adds word to the forth dictionary
+
+    Args:
+        name: name of word as string
+    Returns:
+        None
+    """
+
+    return
+def pushd(num):
+    """
+    Pushes a number onto the data stack
+
+    Args:
+        num: integer to push onto the data stack
+    Returns:
+        None
+    """
+    
+    return
+def popd():
+    """
+    Pops a number off the data stack
+
+    Args:
+        None
+    Returns:
+        The integer popped off the data stack. If the data stack is empty it will return None. 
+    """
+
+    return
+def execute(name):
+    """
+    Grabs the name and finds the dictionary word then executes
+    
+    Args:
+        name: name of word as string
+    Return:
+        Boolean for success or failure
+    """
+    return
+def or():
+    """
+    Or operation on the data stack.
+    Pops off two numbers from the data stack and performs a bitwise OR
+    and pushes the result onto the data stack.
+
+    Args:
+        None
+    Return:
+        Boolean for success or stack underflow
+    """
+
+    return
